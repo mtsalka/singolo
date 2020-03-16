28e730c1804fba123b290a85cdb0bcaeb9ea4f6b
mtsalka <maryia.miskevich@gmail.com>
2020-03-16 14:17:07 +0100

    feat: sticky header

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+'use strict'
+
+//header
+
 const menu = document.getElementById('menu');
 
 menu.addEventListener('click', (event) => {
     menu.querySelectorAll('a').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 });
+
+
+
+window.onscroll = function() {toStickHeader()};
+
+const header = document.getElementById("nav-header");
+let sticky = header.offsetTop;
+
+function toStickHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+};
